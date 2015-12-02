20e403bb73df0af156736e41350d179fc44d4b2b
Gleb <bboygleb@gmail.com>
2015-12-02 17:34:08 +0300

    Wrote PageRank, not tested

--- PageRank.py
+++ PageRank.py
@@ -1,18 +1,37 @@
 class Page:
     """Page contains name and list of outcome links"""
 
-    def __init__(self, name, out_links):
-        self.name = name
-        self.out_links = out_links
-
-class PageInTime(Page):
-    """PageInTime contains all Page fields, time and number of income links at that time"""
-
-    def __init__(self, page, time):
-        self.name = page.name
-        self.out_links = page.out_links
-        self.time = time
-        self.in_links = 0
+    def __init__(self, links, rank):
+        self.links = links
+        self.rank = rank
 
 def PageRank(pages):
-    pass
+    delta_max = 0.5
+    while delta_max > 0.1:
+        delta_max = 0
+        pages_new = {}
+        for i in pages:
+            pages_new[i] = Page(pages[i].links, 0)
+
+        for i in pages:
+            for j in pages[i].links:
+                pages_new[j].rank += pages[i].rank / len(pages[j].links)
+
+        for i in pages:
+            delta_max = max(delta_max, pages_new[i].rank / pages[i].rank)
+        pages = pages_new
+
+    return sorted(pages, key=lambda x: pages[x].rank)
+
+pages = {}
+file = open('data.txt', 'r')
+
+n = int(file.readline())
+for i in range(n):
+    line = file.readline().split()
+    name = line[0][:-1]
+    links = line[1:]
+    pages[name] = Page(links, 1/n)
+
+pages_ranked = PageRank(pages)
+print pages_ranked
--- Tests/test_tests.py
+++ Tests/test_tests.py
@@ -1,36 +1,36 @@
-import PageRank
+from PageRank import *
 
 def test_one():
-    pages = [
-        PageRank.Page('A', []),
-    ]
-    ranked_pages = PageRank.PageRank(pages)
+    pages = {
+        'A': Page([], 1)
+    }
+    ranked_pages = PageRank(pages)
     assert ranked_pages == ['A']
 
 def test_two():
-    pages = [
-        PageRank.Page('A', []),
-        PageRank.Page('B', ['A'])
-    ]
-    ranked_pages = PageRank.PageRank(pages)
+    pages = {
+        'A': Page([], 1/2),
+        'B': Page(['A', 1/2]),
+    }
+    ranked_pages = PageRank(pages)
     assert ranked_pages == ['A', 'B']
 
 def test_four_easy():
-    pages = [
-        PageRank.Page('A', []),
-        PageRank.Page('B', ['A']),
-        PageRank.Page('C', ['A']),
-        PageRank.Page('D', ['A']),
-    ]
-    ranked_pages = PageRank.PageRank(pages)
+    pages = {
+        'A': Page([], 1/4),
+        'B': Page(['A'], 1/4),
+        'C': Page(['A'], 1/4),
+        'D': Page(['A'], 1/4),
+    }
+    ranked_pages = PageRank(pages)
     assert ranked_pages[:1] == ['A']
 
 def test_four_hard():
-    pages = [
-        PageRank.Page('A', []),
-        PageRank.Page('B', ['A', 'C']),
-        PageRank.Page('C', ['A']),
-        PageRank.Page('D', ['A', 'B', 'C']),
-    ]
-    ranked_pages = PageRank.PageRank(pages)
+    pages = {
+        'A': Page([], 1/4),
+        'B': Page(['A', 'C'], 1/4),
+        'C': Page(['A'], 1/4),
+        'D': Page(['A', 'B', 'C'], 1/4),
+    }
+    ranked_pages = PageRank(pages)
     assert ranked_pages == ['A', 'C', 'B', 'D']
